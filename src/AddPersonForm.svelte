bc714fa28db9963bef3a6a6a71df89603c9a6feb
JiriJAdam <jiriadam29@protonmail.com>
2020-11-08 20:00:25 -0600

    added README

---
+++ src/AddPersonForm.svelte
@@ -0,0 +1,46 @@
+<script>
+    import { createEvenDispatcher } from 'svelte';
+
+    let dispatch = createEvenDispatcher();
+
+    let name;
+    let beltColour;
+    let age;
+    let fighting = false;
+    let sneaking = false;
+    let running = false;
+
+    const handleSubmit = () => {
+        console.log(name, beltColour, age, fighting, sneaking, running);   
+        const person = {
+            name,
+            beltColour,
+            age,
+            skills,
+            id: Math.random()
+        }
+        dispatch('addPerson', person);
+    };
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+		<input type="text" placeholder="name" bind:value={name}>
+        <input type="text" placeholder="belt colour" bind:value={beltColour}>
+        <input type="number" bind:value={age}>
+        <label>Skills:</label>
+        <input type="checkbox" bind:checked={fighting}>fighting<br>
+        <input type="checkbox" bind:checked={sneaking}>sneaking<br>
+        <input type="checkbox" bind:checked={running}>running<br>
+        <label>Belt colour</label>
+        <select bind:value={beltColour}>
+            <option value="black">black</option>
+            <option value="orange">orange</option>
+            <option value="brown">brown</option>
+            <option value="white">white</option>
+        </select>
+		<button>Add Person</button>
+</form>
+
+<style>
+
+</style>
---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+	import Modal from './Modal.svelte';
+	import AddPersonForm from './AddPersonForm.svelte';
+
+	let showModal = false;
+
+	const toggelModal = () => {
+		showModal = !showModal;
+	}
+
+	let people = [
+		{ name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+		{ name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+		{ name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+	];
+
+	const handleClick = (id) => {
+		// delete the person from people
+		people = people.filter((person) => person.id != id);
+	}
+
+	const addPerson = (e) => {
+		//console.log(e.detail);
+		const person = e.detail;
+		people = [person, ...people];
+		showModal = false;
+	};
+</script>
+
+<Modal {showModal} on:click={toggelModal}>
+	<AddPersonForm on:addPerson={addPerson}/>
+</Modal>
+<main>
+	<button on:click|once={toggelModal}>Open Modal</button>
+	{#each people as person (person.id)}
+		<div>
+			<h4>{person.name}</h4>
+			{#if person.beltColour === 'black'}
+				<p><strong>MASTER NINJAS</strong></p>
+			{/if}
+			<p>{person.age} years old, {person.beltColour} belt.</p>
+			<button on:click={() => handleClick(person.id)}>delete</button>
+		</div>
+	{:else}
+		<p>There are no people to show...</p>
+	{/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
